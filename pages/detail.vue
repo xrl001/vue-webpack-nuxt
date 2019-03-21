4cd442112f555d7e9e4425c7cb22f9d0dcc3e599
xrl001 <1107232507@qq.com>
2019-03-21 15:26:12 +0800

    Update detail.vue

--- pages/detail.vue
+++ pages/detail.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
-    详情页
-      <router-link tag="a" :to='{name:"index"}'>返回主页</router-link>
+    详情页<br />
+      <router-link tag="a" :to='{name:"index"}'>返回主页</router-link>&nbsp;&nbsp;
       <router-link tag="a" :to='{name:"list"}'>返回列表</router-link>
   </section>
 </template>
